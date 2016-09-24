2697ce2b660bab6e02597657f77e22e8d2f6314a
Frank Lemanschik <frank@dspeed.eu>
2016-09-24 17:25:44 +0100

    Updated

--- test/index.js
+++ test/index.js
@@ -1,11 +1,11 @@
 var assert = require('assert')
 var envy = require('..')
 
-describe('envy-json', function(){
+describe('dssrv-envy-json', function(){
 
   it('accepts a JSON string as input', function(){
-    var config = "{\"basicAuth\": \"$HARP_AUTH\"}"
-    process.env.HARP_AUTH = "foo:bar"
+    var config = "{\"basicAuth\": \"$DSSRV_AUTH\"}"
+    process.env.DSSRV_AUTH = "foo:bar"
     assert.equal(envy(config).basicAuth, "foo:bar")
   })
 
@@ -16,18 +16,18 @@ describe('envy-json', function(){
   })
 
   it('accepts a filename as input', function(){
-    var config = envy(__dirname+"/fixtures/harp.json")
+    var config = envy(__dirname+"/fixtures/dssrv.json")
     assert.equal(config.name, "My Harp App")
   })
 
   it('replaces values starting with $ with their process.env equivalent', function(){
     var config = {
       name: "My Harp App",
-      basicAuth: "$HARP_AUTH",
+      basicAuth: "$DSSRV_AUTH",
       environment: "$NODE_ENV"
     }
 
-    process.env.HARP_AUTH = "gibraltar:snoozleton"
+    process.env.DSSRV_AUTH = "gibraltar:snoozleton"
     process.env.NODE_ENV = "production"
 
     assert.equal(envy(config).name, "My Harp App")
